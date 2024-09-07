a5e70574a9b2ab9705a8fc289928f659b9f3644f
mohamed khairy <mohamed khairy>
2024-09-07 20:19:06 +0300

    finished project

--- script.js
+++ script.js
@@ -22,11 +22,21 @@ let tasks = [
   },
 ];
 
+const getTasksFromStorage = () => {
+  let retrievedTasks = JSON.parse(localStorage.getItem('tasks'));
+
+  tasks = retrievedTasks || [];
+};
+
+getTasksFromStorage();
+
 const fillTasksOnThePage = () => {
   alltasks.innerHTML = '';
 
+  let index = 0;
+
   for (task of tasks) {
-    let content = `<div id="task" class='task'>
+    let content = `<div  id="task" class='task ${task.isDone ? 'done' : ''}'>
                 <div style='width: 70%'>
                   <h2>${task.title}</h2>
   
@@ -43,6 +53,7 @@ const fillTasksOnThePage = () => {
                 </div>
              
                 <div
+                  id="${index}"
                   style='
                     display: flex;
                     justify-content: space-between;
@@ -51,21 +62,42 @@ const fillTasksOnThePage = () => {
                   '
                 >
                   <button
+                  onclick="deleteTask(${index})"
                     id="delete"
                     class='circular'
                     style='background-color: rgb(114, 0, 0); color: white'
                   >
                     <span class='material-symbols-outlined'> delete </span>
                   </button>
-                  <button
-                    onclick="deleteTask()"
+                  ${
+                    task.isDone
+                      ? `
+                      <button
+                    onclick="toggleTaskCompletion(${index})"
+                    id='check'
+                    class='circular'
+                    style='background-color: rgb(114, 0, 0); color: white'
+                  >
+                   <span class="material-symbols-outlined">
+close
+</span>
+                  </button>
+                    
+                    `
+                      : `
+                     <button
+                    onclick="toggleTaskCompletion(${index})"
                     id='check'
                     class='circular'
                     style='background-color: rgb(0, 150, 30); color: white'
                   >
                     <span class='material-symbols-outlined'> check </span>
                   </button>
+                    `
+                  }
+                 
                   <button
+                  onclick="editTask(${index})"
                     id="edit"
                     class='circular'
                     style='background-color: rgb(0, 16, 197, 0.692); color: white'
@@ -77,6 +109,7 @@ const fillTasksOnThePage = () => {
               </div>`;
 
     alltasks.innerHTML += content;
+    index++;
   }
 };
 
@@ -92,10 +125,50 @@ add.addEventListener('click', () => {
     date: date,
     isDone: false,
   };
-  if (task) {
+  if (taskname == '') {
+    return;
+  } else if (taskname) {
     tasks.push(takobj);
+    storeTasks();
     fillTasksOnThePage();
   }
 });
 
-const deleteTask = () => {};
+const deleteTask = (index) => {
+  let task = tasks[index];
+  let confirmation = confirm('are you sure you want to delete ' + task.title);
+  if (confirmation) {
+    tasks.splice(index, 1);
+    storeTasks();
+    fillTasksOnThePage();
+  }
+};
+
+const editTask = (index) => {
+  let task = tasks[index];
+  let newTaskName = prompt('what is the new name: ', task.title);
+
+  if (newTaskName === '') {
+    return task.title;
+  } else if (newTaskName) {
+    task.title = newTaskName;
+  } else if (!newTaskName) {
+    return task.title;
+  }
+
+  task.title = newTaskName;
+  storeTasks();
+  fillTasksOnThePage();
+};
+
+const toggleTaskCompletion = (index) => {
+  let task = tasks[index];
+  task.isDone = !task.isDone;
+  storeTasks();
+  fillTasksOnThePage();
+};
+
+const storeTasks = () => {
+  let tasksString = JSON.stringify(tasks);
+  localStorage.setItem('tasks', tasksString);
+};
